7e580d6fed1dcd3fe000dae41206dc7be3b50588
Natix <96585803+Natix1@users.noreply.github.com>
2023-05-14 12:05:46 +0200

    Optimized count.py

--- count.py
+++ count.py
@@ -1,23 +1,17 @@
 import string
-s=0
-a=0
-d=0
-l=0
-u=0
-r=input("enter any digit, character\n")
-for i in range(len(r)):
-    if(r[i].isspace()):
-        s=s+1       
-    elif(r[i].isalpha()):
-        a=a+1
-        if(r[i].islower()):
-            l=l+1
+
+def main():
+    counts = {}  # Engaging a dictionary
+    text = input("Enter a string: ")
+    for character in text:  # Loop thru each character in the provided string
+        if character in counts:  # Checks if the character is already in the dictionary
+            counts[character] += 1  # If it is, increment the count
         else:
-            u=u+1
-    elif(r[i].isdigit()):
-        d=d+1
-print("No of spaces",s)            
-print("No of alphabets",a)
-print("No of lowercase",l)
-print("No of uppercase",u)
-print("No of digits",d)
+            counts[character] = 1  # Else add it and set the count to 1
+
+    for character, count in counts.items(): # Print the results to the console
+        print(f"'{character}' appears {count} times.")
+
+
+if __name__ == '__main__':
+    main() # This if __name__ == '__main__' check will prevent the script from running if imported thru another file
